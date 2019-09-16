c4fd7bd5eee6b1aa3a51c48585647f504e4ff68b
noah lee <noahlee@cloudcash.kr>
2019-09-16 16:24:31 +0900

    studygram front nuxt js

---
+++ components/LoginBody.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="body">
+    <Top/>
+    <div class="container no-padding">
+      <div class="d-flex justify-content-center align-middle">
+        <div class="card" style="width: 450px; padding: 30px; margin-top: 6rem; margin-left: 1rem; margin-right: 1rem;">
+          <div style="color: #845ef7; font-size: 1.3rem;font-family: 'Quicksand', sans-serif; letter-spacing: 0.7px; font-weight: bold; margin-bottom: 1rem;">
+              스터디그램
+          </div>
+          <div class="form-group">
+            <small>아이디</small>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+<!--            <small id="emailHelp" class="form-text text-muted">으하하하하하</small>-->
+          </div>
+          <div class="form-group">
+            <small>비밀번호</small>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+          </div>
+          <button type="submit" class="btn btn-primary" style="margin-top: 1rem; margin-bottom: 1rem;">로그인</button>
+          <button type="submit" class="btn btn-success">회원가입</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "LoginBody"
+    }
+</script>
+
+<style scoped>
+  .body {
+    margin-top: 4rem;
+    /*margin-bottom: 1rem;*/
+  }
+</style>
---
+++ pages/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <MenuBar/>
+    <LoginBody/>
+    <div class="fixed-bottom">
+      <Footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AppLogo from '~/components/AppLogo.vue'
+  import Header from '~/components/Header.vue'
+  import MenuBar from '~/components/MenuBar.vue'
+  import Body from '~/components/Body.vue'
+  import Footer from '~/components/Footer.vue'
+  import LoginBody from '~/components/LoginBody.vue'
+
+  export default {
+    components: {
+      AppLogo,
+      Header,
+      MenuBar,
+      Body,
+      Footer,
+      LoginBody,
+    }
+  }
+</script>
+
+<style>
+
+  .no-margin {
+    margin: 0px;
+  }
+
+</style>
+
